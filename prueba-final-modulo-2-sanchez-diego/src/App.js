843c9a7a47a4bd4f0833f66ff37222d8559e4fe6
Diego Sanchez <diegominepro@gmail.com>
2020-08-13 15:44:19 -0300

    agregue folders

--- prueba-final-modulo-2-sanchez-diego/src/App.js
+++ prueba-final-modulo-2-sanchez-diego/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button , AppBar , Typography , Toolbar } from '@material-ui/core';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar position="fixed" color="primary">
+      <Toolbar>
+        <Typography variant="h6">
+          
+        </Typography>
+        <Button>Share</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
