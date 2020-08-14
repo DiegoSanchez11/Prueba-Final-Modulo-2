c716392e7da4c59fab7e3cbc2e3cc5004c293e1b
Diego Sanchez <diegominepro@gmail.com>
2020-08-14 11:37:56 -0300

    retoques

--- prueba-final-modulo-2-sanchez-diego/src/components/Card-Login.js
+++ prueba-final-modulo-2-sanchez-diego/src/components/Card-Login.js
@@ -10,15 +10,16 @@ class CardLogin extends React.Component{
             <Card>
                 <TextField id="outlined-basic" label="Outlined" variant="Correo Electronico"/>
                 <TextField id="outlined-basic" label="Outlined" variant="Contraseña" />
+                <Button variant="contained" color="secundary">COMENZAR A CREAR PLAYLUST</Button>
                 <Typography>
                     <Link href="#" onClick={preventDefault} color="inherit">
-                        {'color="inherit"'}
+                        {'color="inherit"'}¿Has ovlidado la contraseña?
                     </Link>
                     <Link href="#" onClick={preventDefault} varitant="body2">
-                        {'variant="body2"'}
+                        {'variant="body2"'}¿NO TIENES CUENTA?
                     </Link>
                 </Typography>
-                <Button></Button>
+                
             </Card>
         )
     }
