b19810dd7c205a709fb98744c5867a7798ce065a
Diego Sanchez <diegominepro@gmail.com>
2020-08-14 10:11:02 -0300

    add new component

--- prueba-final-modulo-2-sanchez-diego/src/components/Header.js
+++ prueba-final-modulo-2-sanchez-diego/src/components/Header.js
@@ -1,14 +1,28 @@
 // icon-h1-img
-import React from 'react';
-import {  } from '@material-ui/core';
-//Css
+import React from "react";
+import { AppBar, Typography, Avatar, Grid } from "@material-ui/core";
 
-class Header extends React.Component{
-    render(){
-        return(
-            <Icon />
-            <h1>{this.props.namePage}</h1>
-            <img />
-        )
+//Css
+import "../styles/Header.css";
+
+class Header extends React.Component {
+    render() {
+        return (
+        <div className="container">
+            <div className="row">
+                <div>
+                    <AppBar className="appBar__box" position="fixed" color="primary">
+                        <img alt="icon" src="" />
+                        <Typography variant="h6">
+                            <h1>mateify</h1>
+                        </Typography>
+                        <Avatar alt="" src="" />
+                    </AppBar>
+                </div>
+            </div>
+        </div>
+        );
     }
 }
+
+export default Header;
