dcf4296d585c128da4e1f4710ae987e0d3f63560
Diego Sanchez <diegominepro@gmail.com>
2020-08-14 12:10:15 -0300

    create component

--- prueba-final-modulo-2-sanchez-diego/src/components/CardLogin.js
+++ prueba-final-modulo-2-sanchez-diego/src/components/CardLogin.js
@@ -2,25 +2,24 @@ import React from 'react';
 import { Button, TextField, Typography, Link , Card } from '@material-ui/core';
 
 // Css
-
+import '../styles/CardLogin.css';
 
 class CardLogin extends React.Component{
     render(){
         return(
             <div className="cardLogin__box">
-                <Card>
-                    <TextField variant="outlined" value="Correo Electronico"/>
-                    <TextField variant="outlined" value="Contraseña" />
-                    <Button variant="contained" color="secundary">COMENZAR A CREAR PLAYLUST</Button>
-                    <Typography>
-                        <Link href="#" onClick="" color="inherit">
-                            ¿Has ovlidado la contraseña?
-                        </Link>
-                        <Link href="#" onClick="" varitant="inherit">
-                            ¿NO TIENES CUENTA?
-                        </Link>
-                    </Typography>
-                </Card>
+                <TextField variant="outlined" value="Correo Electronico"/>
+                <TextField variant="outlined" value="Contraseña" />
+                <Button variant="contained" color="secondary">COMENZAR A CREAR PLAYLIST</Button>
+                <Typography>
+                    <Link href="#" onClick="" color="inherit">
+                        ¿Has ovlidado la contraseña?
+                    </Link>
+                    <Link href="#" onClick="" varitant="inherit">
+                        ¿NO TIENES CUENTA?
+                    </Link>
+                </Typography>
+                <Button variant="outlined" color="default">REGISTRATE</Button>
             </div>
             
         )
