9ccd90c9d1958bf382af92d751cff18c39c75078
Diego Sanchez <diegominepro@gmail.com>
2020-08-14 14:12:03 -0300

    retoque

--- prueba-final-modulo-2-sanchez-diego/src/components/Form.js
+++ prueba-final-modulo-2-sanchez-diego/src/components/Form.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { TextField } from '@material-ui/core';
+import { TextField, Paper } from '@material-ui/core';
+// Components
+import Button from './Button';
+import Links from './Links';
 
 class Form extends React.Component{
     render(){
         return(
             <div>
-                <TextField variant="outlined" placeholder="Correo Electronico"/>
-                <TextField variant="outlined" placeholder="Contraseña" />
+                <Paper>
+                    <TextField variant="outlined" placeholder="Correo Electronico"/>
+                    <TextField variant="outlined" placeholder="Contraseña" />
+                    <Button button_variant="contained" button_color="secondary" button_name="COMENZAR A CREAR PLAYLIST"/>
+                    <Links />
+                    <Button button_variant="outlined" button_color="default" button_name="REGISTRATE" />
+                </Paper>
             </div>
         )
     }
