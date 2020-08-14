d94169ce2ab0f429459084942f1137ef5cd16c2d
Diego Sanchez <diegominepro@gmail.com>
2020-08-14 11:51:48 -0300

    edit

--- prueba-final-modulo-2-sanchez-diego/src/components/CardLogin.js
+++ prueba-final-modulo-2-sanchez-diego/src/components/CardLogin.js
@@ -7,20 +7,22 @@ import { Button, TextField, Typography, Link , Card } from '@material-ui/core';
 class CardLogin extends React.Component{
     render(){
         return(
-            <Card>
-                <TextField id="outlined-basic" label="Outlined" variant="Correo Electronico"/>
-                <TextField id="outlined-basic" label="Outlined" variant="Contraseña" />
-                <Button variant="contained" color="secundary">COMENZAR A CREAR PLAYLUST</Button>
-                <Typography>
-                    <Link href="#" onClick={preventDefault} color="inherit">
-                        {'color="inherit"'}¿Has ovlidado la contraseña?
-                    </Link>
-                    <Link href="#" onClick={preventDefault} varitant="body2">
-                        {'variant="body2"'}¿NO TIENES CUENTA?
-                    </Link>
-                </Typography>
-                
-            </Card>
+            <div className="cardLogin__box">
+                <Card>
+                    <TextField variant="outlined" value="Correo Electronico"/>
+                    <TextField variant="outlined" value="Contraseña" />
+                    <Button variant="contained" color="secundary">COMENZAR A CREAR PLAYLUST</Button>
+                    <Typography>
+                        <Link href="#" onClick="" color="inherit">
+                            ¿Has ovlidado la contraseña?
+                        </Link>
+                        <Link href="#" onClick="" varitant="inherit">
+                            ¿NO TIENES CUENTA?
+                        </Link>
+                    </Typography>
+                </Card>
+            </div>
+            
         )
     }
 }
