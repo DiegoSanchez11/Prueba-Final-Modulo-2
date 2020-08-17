1c9367882eb1e946e477f7d8e90ecd00dee01f60
Diego Sanchez <diegominepro@gmail.com>
2020-08-17 16:15:54 -0300

    creando

---
+++ prueba-final-modulo-2-sanchez-diego/src/components/MainTextForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {  } from '@material-ui/core';
+
+// Css
+import '../styles/MainTextForm.css';
+
+export const MainTextForm = () => {
+    return(
+        <div>
+            <div className="text__icon"></div>
+            <h1>Ingresar</h1>
+        </div>
+}
