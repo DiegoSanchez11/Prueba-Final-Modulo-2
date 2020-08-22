1b412a0489fceaa8bec357f5d41e3df4be04f271
Diego Sanchez <diegominepro@gmail.com>
2020-08-21 23:07:24 -0300

    add icon

--- prueba-final-modulo-2-sanchez-diego/src/components/Form.js
+++ prueba-final-modulo-2-sanchez-diego/src/components/Form.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import { TextField, Paper } from '@material-ui/core';
+import React from "react";
+import { TextField, Paper } from "@material-ui/core";
 // Components
-import Boton from './Boton';
-import Links from './Links';
+import Boton from "./Boton";
+import Links from "./Links";
 // Css
-import '../styles/Form.css';
+import "../styles/Form.css";
 
-class Form extends React.Component{
-    render(){
-        return(
-            <div className="form__box">
-                <Paper className="form__paper">
-                    <TextField variant="outlined" placeholder="Correo Electronico"/>
-                    <TextField variant="outlined" placeholder="Contraseña"/>
-                    <Boton button_variant="contained" button_color="secondary" button_name="COMENZAR A CREAR PLAYLIST" />
-                    <Links />
-                    <Boton button_variant="outlined" button_color="default" button_name="REGISTRATE"/>
-                </Paper>
-            </div>
-        )
-    }
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="form__box">
+        <Paper className="form__paper">
+          <TextField className="form__input" variant="outlined" placeholder="Correo Electronico" />
+          <TextField className="form__input" variant="outlined" placeholder="Contraseña" />
+          <Boton
+            button_variant="contained"
+            button_color="secondary"
+            button_name="COMENZAR A CREAR PLAYLIST"
+          />
+          <Links />
+          <Boton
+            button_variant="outlined"
+            button_color="default"
+            button_name="REGISTRATE"
+          />
+        </Paper>
+      </div>
+    );
+  }
 }
 
 export default Form;
--- prueba-final-modulo-2-sanchez-diego/src/pages/HomeLogin.js
+++ prueba-final-modulo-2-sanchez-diego/src/pages/HomeLogin.js
@@ -1,21 +1,20 @@
 // Este sera mi Home-Login
-import React from 'react';
+import React from "react";
 // Components
-import Form from '../components/Form';
-import NavBar from '../components/NavBar';
-import MainTextForm from '../components/MainTextForm';
+import Form from "../components/Form";
+import NavBar from "../components/NavBar";
+import MainTextForm from "../components/MainTextForm";
 
-class HomeLogin extends React.Component{
-    render(){
-        return(
-            <div>
-                
-                <NavBar />
-                <MainTextForm />
-                <Form />
-            </div>
-        )
-    }
+class HomeLogin extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <MainTextForm />
+        <Form />
+      </div>
+    );
+  }
 }
 
 export default HomeLogin;
