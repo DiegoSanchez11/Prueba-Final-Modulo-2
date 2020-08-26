17bab6a6ea40657bbcd76fc7e58f64d8dc6d5e77
Diego Sanchez <diegominepro@gmail.com>
2020-08-26 16:33:27 -0300

    retocando

--- prueba-final-modulo-2-sanchez-diego/src/components/Form.js
+++ prueba-final-modulo-2-sanchez-diego/src/components/Form.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { TextField, Paper } from "@material-ui/core";
 import { Router , Route ,Link ,Swutch ,NavLink } from 'react-router-dom';
+
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import NavigationIcon from '@material-ui/icons/Navigation';
+
+
+
 // Components
 import Boton from "./Boton";
 import Links from "./Links";
@@ -11,7 +22,13 @@ import users from '../baseDeDatos/userData.json';
 // Css
 import "../styles/Form.css";
 
-const  goToPage = (<Playlist />)
+const  goToPage = () => {
+  return(
+    <Link to="/Playlist">
+      ("../pages/Playlist.js");
+    </Link>
+  )
+}
 
 // Colocar el code para la verificacion de usuarios
 
@@ -34,7 +51,7 @@ class Form extends React.Component {
             button_variant="contained"
             button_color="secondary"
             button_name="COMENZAR A CREAR PLAYLIST"
-            href={goToPage}
+            href="/Playlist"
           />
           <Links />
           <Boton
